0b13313281a9b1a87d7f40069a18e7c7b1158542
viet2 <viet2@DESKTOP-5FNGVRJ>
2024-07-25 10:21:13 +0700

    check password register

--- Models/DTOs/Account/RegisterRequest.cs
+++ Models/DTOs/Account/RegisterRequest.cs
@@ -13,6 +13,8 @@ namespace Models.DTOs.Account
 
         [Required]
         [MinLength(6)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$",
+        ErrorMessage = "Password must be at least 6 characters long and contain at least one lowercase letter, one uppercase letter, one digit, and one special character.")]
         public string Password { get; set; }
 
         [Required]
